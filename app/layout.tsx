0b6509d363383822d8189a09f10860fb1866cba7
jewelryde <jewelrydecornerforwork@gmail.com>
2025-10-21 18:10:46 +0800

    Update Content

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NDLZQ2HE5X"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-NDLZQ2HE5X');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
