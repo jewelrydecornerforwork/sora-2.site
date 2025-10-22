14310354841698c722238d7145b947a675822989
jewelryde <jewelrydecornerforwork@gmail.com>
2025-10-22 10:49:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NDLZQ2HE5X"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-NDLZQ2HE5X');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
